c335f2db90d24cdfd03a32e4bdadc89fb1d51575
Nhu Le <ataruhihi@gmail.com>
2018-09-26 23:34:39 +0700

    Services > Show messages: code > Send a message from HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,6 +12,8 @@ export class HeroService {
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
