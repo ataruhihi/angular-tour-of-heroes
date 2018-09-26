2e90fe57cfcc4041154cceac659928084b69e0de
Nhu Le <ataruhihi@gmail.com>
2018-09-26 23:00:22 +0700

    Services > Update HeroesComponent: code > Inject the HeroService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
  
 @Component({
   selector: 'app-heroes',
@@ -10,10 +10,10 @@ import { HEROES } from '../mock-heroes';
  
 export class HeroesComponent implements OnInit {
  
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
  
-  constructor() { }
+  constructor(private heroService: HeroService) { }
  
   ngOnInit() {
   }
