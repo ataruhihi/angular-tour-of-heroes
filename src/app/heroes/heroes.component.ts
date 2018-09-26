3dc5adb360771f84c47c94be999a4c5a4a15e248
Nhu Le <ataruhihi@gmail.com>
2018-09-26 23:02:40 +0700

    Services > Update HeroesComponent: code > Call it in ngOnInit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -16,6 +16,7 @@ export class HeroesComponent implements OnInit {
   constructor(private heroService: HeroService) { }
  
   ngOnInit() {
+    this.getHeroes();
   }
  
   onSelect(hero: Hero): void {
