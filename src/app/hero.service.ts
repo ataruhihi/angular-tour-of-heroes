0d7e7b260470be0e42fd33ccb5f0cf8469231b47
Nhu Le <ataruhihi@gmail.com>
2018-09-27 09:14:03 +0700

    HTTP > Heroes and HTTP: code > Get hero by id

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,7 +5,6 @@ import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
  
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
 @Injectable({ providedIn: 'root' })
@@ -26,10 +25,13 @@ export class HeroService {
       );
   }
  
+  /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
-    // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   /**
