5655f7e1660bfbb2545cf3dd1bb60cfa6b05f86d
Nhu Le <ataruhihi@gmail.com>
2018-09-27 08:28:53 +0700

    Routing > Routable HeroDetailComponent: code > Find the way back

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,23 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Hero } from '../hero';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-
+ 
+import { Hero }         from '../hero';
 import { HeroService }  from '../hero.service';
  
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
+  hero: Hero;
  
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location
-  ) {}  
+  ) {}
  
   ngOnInit(): void {
     this.getHero();
@@ -28,5 +28,8 @@ export class HeroDetailComponent implements OnInit {
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
-  
+ 
+  goBack(): void {
+    this.location.back();
+  }
 }
