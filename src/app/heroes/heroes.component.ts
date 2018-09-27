8e9c60dc44f6ddc2b06847b5c5c46457d30dd467
Nhu Le <ataruhihi@gmail.com>
2018-09-27 07:00:00 +0700

    Routing > Navigating to hero details: code > Remove dead code (optional)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,22 +9,16 @@ import { HeroService } from '../hero.service';
 })
  
 export class HeroesComponent implements OnInit {
- 
   heroes: Hero[];
-  selectedHero: Hero;
- 
+
   constructor(private heroService: HeroService) { }
- 
+
   ngOnInit() {
     this.getHeroes();
-  }
- 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
