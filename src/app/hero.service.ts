f0784291899618e1dc5aaa7cd1d58e3a87a8d9dd
Nhu Le <ataruhihi@gmail.com>
2018-09-27 08:55:31 +0700

    HTTP > Heroes and HTTP: code > Get heroes with HttpClient

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,10 +16,9 @@ export class HeroService {
     private http: HttpClient,
     private messageService: MessageService) { }
  
-  getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+  /** GET heroes from the server */
+  getHeroes (): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
  
   getHero(id: number): Observable<Hero> {
